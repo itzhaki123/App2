26d69350af4eab0ac555e900a9c184fcabd55fc9
itzhakiamit0@gmail.com <itzhakiamit0@gmail.com>
2023-11-29 22:27:17 +0200

    ***

---
+++ GameEngine1/Services.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine1
+{
+    internal class Services
+    {
+    }
+}
