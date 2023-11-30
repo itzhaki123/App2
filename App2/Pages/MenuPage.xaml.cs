8f06b57ac30fbd2acb4a03f76cdd0d4e11381967
m e <mataneldar123@gmail.com>
2023-11-30 10:49:55 +0200

    ***

--- App2/Pages/MenuPage.xaml.cs
+++ App2/Pages/MenuPage.xaml.cs
@@ -21,7 +21,7 @@ using Windows.UI.Xaml.Navigation;
 namespace App2
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// An empty page that can be used on its own or navigated to within a Frame. 
     /// </summary>
     public sealed partial class MenuPage : Page
     {
