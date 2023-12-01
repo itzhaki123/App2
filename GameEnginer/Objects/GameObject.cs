60b333d18f69dd2bad28f5e3d24ba8ed45653a6c
m e <mataneldar123@gmail.com>
2023-12-01 11:49:07 +0200

    ***

--- GameEnginer/Objects/GameObject.cs
+++ GameEnginer/Objects/GameObject.cs
@@ -1,13 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GameEnginer.Services;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
 
 namespace GameEnginer.Objects
 {
     public abstract class GameObject
     {
+        protected double _X; //מיקום נוכחי
+        protected double _Y; //מיקום נוכחי
 
+        protected double _placeX; //מיקום התחלתי
+        protected double _placeY; //מיקום התחלתי
+
+        public Image Image { get; set; }
+        protected string _fileName;
+
+        public double Width => Image.ActualWidth;
+
+        public double Height => Image.ActualHeight;
+
+        public virtual Rect Rect => new Rect(_X, _Y, Width, Height); //המלבן שמקיף את העצם
+
+        public bool Collisional { get; set; } = true; //אפשר להתנגש בו, לא שקוף
+
+        protected Scene scene; //זירת המשחק
     }
 }
