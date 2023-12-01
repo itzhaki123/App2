dc1f1d8b639f0b43c9236d64526e46525f0a7817
m e <mataneldar123@gmail.com>
2023-12-01 11:29:51 +0200

    ***

---
+++ App2/Objects/Jelly.cs
@@ -0,0 +1,13 @@
+﻿using GameEnginer.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2.Objects
+{
+    public class Jelly:GameObject
+    {
+    }
+}
---
+++ App2/Services/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using GameEnginer.Services;
+
+namespace App2.Services
+{
+    public class GameManager:Manager
+    {
+
+    }
+}
