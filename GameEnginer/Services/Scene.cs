cd66de13546a2403e87cf898e11f5e9dd7900cdd
m e <mataneldar123@gmail.com>
2024-01-04 11:11:37 +0200

    ***

--- GameEnginer/Services/Scene.cs
+++ GameEnginer/Services/Scene.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace GameEnginer.Services
@@ -18,11 +19,30 @@ namespace GameEnginer.Services
         /// </summary>
         private List<GameObject> _gameObjects = new List<GameObject>();//מאגר האובייקטים במשחק 
 
+        protected List<GameObject> _gameObjectsSnapshot => _gameObjects.ToList();
+
         public double Ground { get; set; }//רצפה
 
         public Scene()
         {
             Manager.Events.OnRun += Run;
+            Manager.Events.OnRun += CheckCollisional;
+        }
+
+        private void CheckCollisional()
+        {
+            foreach(var gameObject in _gameObjectsSnapshot) //עוברים על כל רשימת האוובייקטים
+            {
+                if(gameObject.Collisional) // אם האובייקט לא שקוף
+                {
+                    var otherObject = _gameObjectsSnapshot.FirstOrDefault(g => !ReferenceEquals(g, gameObject) && g.Collisional &&
+                    !RectHelper.Intersect(g.Rect, gameObject.Rect).IsEmpty);
+                    if (otherObject != null)
+                    {
+                        gameObject.Collide(otherObject);
+                    }
+                }
+            }
         }
 
         private void Run()
