37400c3411b0dfdf76bf6adbbbadc8854afe4a93
itzhakiamit0@gmail.com <itzhakiamit0@gmail.com>
2024-02-18 18:59:04 +0200

    ***

--- App2/Objects/Obstacle1.cs
+++ App2/Objects/Obstacle1.cs
@@ -34,7 +34,7 @@ namespace App2.Objects
                 _X = GenerateRandomNumberInRange(3200, 3700);
                 _dX -= 1.6;
                 check += 1;
-                if (check > 18)
+                if (check > 19)
                 {
                     _dX += 1.6;
                 }
