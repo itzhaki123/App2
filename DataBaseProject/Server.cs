88db86d1b130609bee097a16d4884fc54128f9f8
itzhaki123 <itzhakiamit0@gmail.com>
2024-03-21 11:14:33 +0200

    ***

--- DataBaseProject/Server.cs
+++ DataBaseProject/Server.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +11,27 @@ namespace DataBaseProject
 {
     public static class Server
     {
-        private static string dbPath = ApplicationData.Current.LocalFolder.Path;
-        private static string connectionString = "Filename=" + dbPath + "\\DBGame.db";
+        private static string dbPath = ApplicationData.Current.LocalFolder.Path; //נתיב מסד נתונים במחשב
+        private static string connectionString = "Filename=" + dbPath + "\\DBGame.db"; //המחרוזת שבאמצעותה נוכל להתחבר למסד הנתונים
+        /*
+          הפעולה בודקת אם המשתמש הזין נתונים נכונים ונמצא במאגר משתמשים אם הכל תקין,
+          null של המשתמש אם הנתונים אינם תקינים הפעולה מחזירה ערך UserId הפעולה מחזירה את  
+        */
+        public static int? ValidateUser(string userName, string userPassword)
+        {
+            string query = $"SELECT UserId FROM [User] WHERE UserName = '{userName}' AND UserPassword = '{userPassword}'";
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                SqliteCommand command = new SqliteCommand(query, connection);
+                SqliteDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    return reader.GetInt32(0);
+                }
+                return null;
+            }
+        }
     }
 }
